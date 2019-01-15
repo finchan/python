c2262faab7abb64654000a3d6408d5bc53b4e145
finchan <finchan@outlook.com>
2019-01-15 23:36:18 +0800

    file manipulation

--- headfirstpython/chapter03/filemanipulation.py
+++ headfirstpython/chapter03/filemanipulation.py
@@ -3,5 +3,9 @@ print(data.readline(), end='')
 print(data.readline(), end='')
 data.seek(0)
 for each_line in data:
-    print(each_line, end='')
+    if not each_line.find(":") == -1:
+        (role, words) = each_line.split(":", 1)
+        print(role, end="")
+        print(" said: ", end="")
+        print(words, end="")
 data.close()
