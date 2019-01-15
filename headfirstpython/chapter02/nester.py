7c67dee3f3ab3d751c36b28919e9c1b212ba88da
finchan <finchan@outlook.com>
2019-01-15 21:17:49 +0800

    Merge branch 'master' of https://github.com/finchan/python
    # Conflicts:
    #	headfirstpython/chapter01/list.py
    #	headfirstpython/chapter02/nester.py

--- headfirstpython/chapter02/nester.py
+++ headfirstpython/chapter02/nester.py
@@ -1,13 +1,34 @@
-"""This is the "nester.py" module and it provides one function called print_lol()
-   which prints lists that may or may not include nested lists."""
+"""This is the "nester.py" module and it provides one function called
+    print_lol()which prints lists that may or may not include nested lists."""
 
-def print_lol(a_list):
+
+def print_lol(the_list):
     """This function takes one positional argument called "the_list", which
-            is any Python list (of - possibly - nested lists). Each data item in the
-            provided list is (recursively) printed to the screen on it’s own line."""
-
-    for _each_item in a_list:
-        if isinstance(_each_item, list):
-            print_lol(_each_item)
+        is any Python list (of - possibly - nested lists). Each data item in the
+        provided list is (recursively) printed to the screen on it’s own line."""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
         else:
-            print(_each_item)
+            print(each_item)
+
+
+def print_lol2(the_list, level=0):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol2(each_item, level + 1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end='')
+            print(each_item)
+
+
+def print_lol3(the_list, indent=False, level=0):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol3(each_item, indent, level + 1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
+            print(each_item)
