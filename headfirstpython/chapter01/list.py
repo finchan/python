828285697a2b98a965450117394415757395fe4e
Xavier Ding <finchan_ding@hotmail.com>
2019-01-15 09:26:37 +0800

    list & function & module

--- headfirstpython/chapter01/list.py
+++ headfirstpython/chapter01/list.py
@@ -36,3 +36,50 @@ count = 0
 while count < len(cast):
     print(cast[count])
     count = count + 1
+
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+          ["Graham Chapman", ["Michael Palin", "John Cleese",
+                              "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+print(movies)
+
+for each_item in movies:
+    print(each_item)
+
+for each_item in movies:
+    if isinstance(each_item, list):
+        for nested_item in each_item:
+            print(nested_item)
+    else:
+        print(each_item)
+
+print('_____________________________')
+for each_item in movies:
+    if isinstance(each_item, list):
+        for nested_item in each_item:
+            if isinstance(nested_item, list):
+                for deeper_item in nested_item:
+                    if isinstance(deeper_item, list):
+                        for deepest_item in deeper_item:
+                            print(deepest_item)
+                    else:
+                        print(deeper_item)
+            else:
+                print(nested_item)
+    else:
+        print(each_item)
+
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+          ["Graham Chapman", ["Michael Palin", "John Cleese",
+                              "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+
+def print_lol(a_list):
+    for each_item in a_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+
+
+print_lol(movies)
