2c7cfdfefdf0f1d20ec3546ed167ab819dcfce6f
finchan <finchan@outlook.com>
2019-01-16 23:26:15 +0800

    try except finally with pickle->dump/load

--- headfirstpython/chapter04/filemanipulation_write.py
+++ headfirstpython/chapter04/filemanipulation_write.py
@@ -1,3 +1,4 @@
+import pickle
 from headfirstpython.chapter02.nester import *
 
 man = []
@@ -42,3 +43,23 @@ try:
         print(mf.readline())
 except IOError as err:
     print(str(err))
+
+try:
+    with open('man_data.txt', 'wb') as man_file, open('other_data.txt', 'wb') as other_file:
+        pickle.dump(man, man_file)
+        pickle.dump(other, other_file)
+except IOError as err:
+    print('File Error: ' + str(err))
+except pickle.PickleError as pirr:
+    print('Pickling Error - Dump Error: ' + str(pirr))
+
+try:
+    with open('man_data.txt', 'rb') as man_file:
+        man = pickle.load(man_file)
+        print_lol4(man)
+except IOError as err:
+    print("File Error: " + str(err))
+except pickle.PickleError as pirr:
+    print("Pickling Error: " + str(pirr))
+print(man[0])
+print(man[-1])
