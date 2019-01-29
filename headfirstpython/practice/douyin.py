319246b78a17b226bd3ec4065b050dcd2bad3be0
Xavier Ding <finchan_ding@hotmail.com>
2019-01-29 14:39:00 +0800

    Practice

---
+++ headfirstpython/practice/douyin.py
@@ -0,0 +1,7 @@
+from headfirstpython.practice.download.files import *
+
+# TikTok JSON file and Download Saving Disk
+FILE_NAME = "data/file1.txt"
+SAVE_LOCATION = "E://Tidok//"
+
+do_retrieve_all_files(FILE_NAME, SAVE_LOCATION)
